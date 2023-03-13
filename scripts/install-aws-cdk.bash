441822a90355851f9d3f7cf733d97e9ac1478a6a
mreynolds <mreynolds@users.noreply.github.com>
2023-03-12 21:24:25 -0700

    Added, and cleaned up tmp files

--- scripts/install-aws-cdk.bash
+++ scripts/install-aws-cdk.bash
@@ -4,14 +4,16 @@
 
 TARGET="v18.15.0"
 
+DIR=$(mktemp -d)
+pushd ${DIR}
 curl -L https://nodejs.org/dist/${TARGET}/node-${TARGET}-linux-x64.tar.xz -o target.tar.xz
-mkdir -p local/node/
-tar -xf target.tar.xz -C local/node --strip-components=1
+mkdir -p /aws/local/node/
+tar -xf target.tar.xz -C /aws/local/node --strip-components=1
 ln -s /aws/local/node/bin/node /aws/bin
 ln -s /aws/local/node/bin/npm /aws/bin
-ls -la bin
-ls -la local/node/bin/
-
 
 PATH=${PATH}:/aws/bin npm install -g aws-cdk
 ln -s /aws/local/node/bin/cdk /aws/bin
+
+popd
+rm -rf ${DIR}
--- scripts/install-aws-cli.bash
+++ scripts/install-aws-cli.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-pushd /tmp/
+DIR=$(mktemp -d)
+pushd ${DIR}
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip -q awscliv2.zip
 ./aws/install --bin-dir /aws/bin/ --install-dir /aws/local/aws-cli/
 rm awscliv2.zip
 popd
+rm -rf ${DIR}
